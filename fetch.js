5ad3e346f3c51574dfe53aa9762a05053d00e5fe
kazinishad <meem.kazi.789@gmail.com>
2024-03-02 00:19:54 +0600

    added loading icon

--- fetch.js
+++ fetch.js
@@ -21,6 +21,16 @@ const diplayPhones= phones =>{
 const phoneContainer = document.getElementById('phone-container');
 //clear phone container cards before adding new cards mane 1st a iphone search korle iphone ashbe abr jodi samsung search kori tahole ihpone er cards gula shoray dibe r just samsung er cards gula display korbe  aita korar jonno  .textContent empty dite hbe.//
 phoneContainer.textContent = '';
+// jodi 10ta ba 12 tar beshi hoy tahole show all button ta dekha jabe r onno shmy dekha jabe nah.aita .slice diar por set korte hbe.aikhane condion dia bole dice j 12 tar beshi hole .remove('hidden') dia calsslist ta cole jabe r 12 tar kom hole .add('hidden') dia abr hide hoya jabe.
+const showAllContainer = document.getElementById('show-all-container');
+if(phones.length >12){
+    showAllContainer.classList.remove('hidden');
+}
+else{
+    showAllContainer.classList.add('hidden');
+}
+// jodi 1st 10 ta ba 12 ta phone ba data dekhate chai tahole .slice(0,10)/.slice(0,12) aivabe dite hbe. aikhane phones paramitar theke data gula ashtece tai phones k call kore .slice korte hbe.
+phones = phones.slice(0,12);
 //--------------------------------//
     
     phones.forEach(phone=>{
@@ -44,12 +54,15 @@ phoneContainer.textContent = '';
         phoneContainer.appendChild(phoneCard);
         
     })
+    //aikhane loading icon hide korar jonno false likhte hbe.
+    toggleLoadingSpiner(false);
 }
 //----------------------------------//
 
 //------------------------------------//
 //handle search button added 
 const handleSearch = () =>{
+    toggleLoadingSpiner(true);
  const searchField = document.getElementById('search-field');
  const searchText = searchField.value;
  //console.log(searchText);
@@ -60,6 +73,19 @@ const handleSearch = () =>{
 }
 //------------------------------------------//
 
+//------------------------------------//
+//loading icon ta shb shmy dekhabe nah tai class name hidden dia rakhte hbe tarpor aikhane eshe akta call back function likhte hbe isloading akta paramitar nite hbe oita if condition a dite hbe.tarpor .remove('hidden') dia class ta shoray dile loading icon ta show hbe abr .add('hidden') dia hide kora hoy. tarpor handle search button er function er moddhe ai function ta call kore paramitar true dite hbe. tarpor upore .append er niche function ta call kore false likhte hbe. 
+const toggleLoadingSpiner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+
+}
+//------------------------------------//
 
 //data load kaj korar jonno function ta k call korte hbe.
 //loadPhone();
